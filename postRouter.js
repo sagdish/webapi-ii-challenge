61e07a1e3f12d0363ae477b8ea6127c27485132a
sagdish <forsi1@yahoo.com>
2019-11-07 19:40:23 +0500

    assigment completed

--- postRouter.js
+++ postRouter.js
@@ -27,47 +27,92 @@ router.route('/:id')
   .get((req, res) => {
     const { id } = req.params;
     db.findById(id)
-      .then(post => res.status(400).json(post))
+      .then(post => {
+        if (post.length < 1) {
+          res.status(404).json({message: "this post doesn't exist"})
+        } else {
+          res.status(400).json(post)
+        }
+      })
       .catch(err => res.status(500).json(err));
   })
+
   .put((req, res) => {
-    
+    const { id } = req.params;
+    const editedPost = req.body;
+    if (!editedPost.title && !editedPost.contents) {
+      res.status(404).json('please provide details');
+    }
+
+    db.update(id, editedPost)
+    .then(response => {
+      if (response > 0) {
+        db.findById(id)
+          .then(post => res.status(400).json(post))
+      }
+    })
+    .catch(err => res.status(500).json(err));
   })
+
   .delete((req, res) => {
-    
+    const { id } = req.params;
+
+    // variable to store post for deletion
+    let postForDeletion;
+
+    db.findById(id)
+      .then(post => {
+        if (post.length < 1) {
+          res.json({message: "this post doesn't exist"})
+        } else {
+          postForDeletion = post[0];
+          db.remove(id)
+          .then(post => {
+            res.status(200).json({deleted: postForDeletion})
+          })
+          .catch(err => res.status(500).json(err));
+        }
+      })
+      .catch(err => res.status(500).json(err));
+
   });
 
-router.route('/:id/comment')
+router.route('/:id/comments')
   .get((req, res) => {
-    
+    const { id } = req.params;
+    db.findPostComments(id)
+      .then(comments => {
+        if (comments.length < 1) {
+          res.status(404).json({message: "there are no comments on that post"})
+        } else {
+          res.status(400).json(comments)
+        }
+      })
+      .catch(err => res.status(500).json(err));
   })
   .post((req, res) => {
-    
+    const { id } = req.params;
+    const newComment = { ...req.body, post_id: id }
+    if (!newComment.text) {
+      res.status(404).json({error: 'Please provide text field'})
+    }
+
+    db.insertComment(newComment)
+      .then(newId => {
+        console.log(newId);
+        db.findCommentById(JSON.stringify(newId.id))
+          .then(comment=> res.status(200).json(comment));
+      })
+      .catch(err => res.status(500).json(err));
+
   });
 
+router.get('/getcomment/:id', (req, res) => {
+  const { id } = req.params;
+  console.log( typeof id )
+  db.findCommentById(id)
+    .then(comment=> res.status(200).json(comment));
+})
 
 
 module.exports = router;
-
-router.route('/')
-  .get((req, res) => {
-    Spot.find()
-      .then(spots => {
-        res.status(200).json(spots);
-      })
-      .catch(err => {
-        res.status(500).json({ error: err });
-      });
-  })
-  .post((req, res) => {
-    const newSpot = req.body;
-    const spot = new Spot(newSpot);
-
-    spot.save()
-      .then(spot => {
-        res.status(201).json(spot);
-      })
-      .catch(err => {
-        res.status(500).json({ error: err });
-      });
-  });
